f279868e7eef44ebf7d53fda503206d99e79ded0
erikmorantegerlund <48085144+erikmorantegerlund@users.noreply.github.com>
2020-11-09 12:53:54 +0100

    Add files via upload

---
+++ Planter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace PlanteCenter
+{
+    public class Planter
+    {
+        private int _id;
+        private string __type;
+        private string _navn;
+        private int _pris;
+        private int _maksHøjde;
+
+        public Planter(int id, string type, string navn, int pris, int maksHøjde)
+        {
+            _id = id;
+            __type = type;
+            _navn = navn;
+            _pris = pris;
+            _maksHøjde = maksHøjde; 
+        }
+
+        public Planter()
+        {
+                
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set { value = _id; }
+        }
+
+        public string Type
+        {
+            get { return __type; }
+            set { value = __type; }
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { value = _navn; }
+        }
+
+        public int Pris
+        {
+            get { return _pris; }
+            set { value = Pris; }
+        }
+
+        public int MaksHøjde
+        {
+            get { return _maksHøjde; }
+            set { value = _maksHøjde; }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_id)}: {_id}, {nameof(__type)}: {__type}, {nameof(_navn)}: {_navn}, {nameof(_pris)}: {_pris}, {nameof(_maksHøjde)}: {_maksHøjde}";
+        }
+    }
+}
---
+++ PlanterContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlanteCenter
+{
+   public class PlanterContext : DbContext
+    {
+        
+        
+            public PlanterContext(DbContextOptions<PlanterContext> options)
+                : base(options)
+            {
+
+            }
+
+            public DbSet<Planter> planters { get; set; }
+        
+    }
+}
